cf75de70945dd1f021211f38aabd1c8ed63e38a7
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-04 00:27:49 -0300

    se programa alarma

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -1,25 +1,42 @@
-const d=document;
+const d = document;
 
-export function relojDigital(reloj,btnPlay, btnStop) {
+export function relojDigital(reloj, btnPlay, btnStop) {
   let relojTiempo;
 
-  d.addEventListener("click", e=>{
-    if(e.target.matches(btnPlay)){
-      relojTiempo=setInterval(() => {
-        let relojHora= new Date().toLocaleTimeString();
-        d.querySelector(reloj).innerHTML=`<h3>${relojHora}</h3>`;
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btnPlay)) {
+      relojTiempo = setInterval(() => {
+        let relojHora = new Date().toLocaleTimeString();
+        d.querySelector(reloj).innerHTML = `<h3>${relojHora}</h3>`;
       }, 1000);
 
-      e.target.disabled=true;
-
-      } 
-    if(e.target.matches(btnStop)){
-      clearInterval(relojTiempo);
-      d.querySelector(reloj).innerHTML=null;
-      d.querySelector(btnPlay).disabled=false;
+      e.target.disabled = true;
     }
-  })
+    if (e.target.matches(btnStop)) {
+      clearInterval(relojTiempo);
+      d.querySelector(reloj).innerHTML = null;
+      d.querySelector(btnPlay).disabled = false;
+    }
+  });
 }
-export function alarma() {
+export function alarma(alarma, btnPlay, btnStop) {
+  let alarmaTiempo;
+  const $alarma = d.createElement("audio");
+  $alarma.src = alarma;
 
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btnPlay)) {
+      alarmaTiempo = setTimeout(() => {
+        $alarma.play();
+      }, 2000);
+
+      e.target.disabled = true;
+    }
+    if (e.target.matches(btnStop)) {
+      clearTimeout(alarmaTiempo);
+      $alarma.pause();
+      $alarma.currentTime = 0;
+      d.querySelector(btnPlay).disabled = false;
+    }
+  });
 }
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,10 +1,10 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
-import { relojDigital,alarma } from "./dom/reloj.js";
+import { relojDigital, alarma } from "./dom/reloj.js";
 
+const d = document;
 
-const d=document;
-
-d.addEventListener("DOMContentLoaded",(e)=>{
-  hamburgerMenu(".panel__btn", ".panel",".panel__menu a");
-  relojDigital("#reloj","#activar-reloj","#desactivar-reloj")
-})
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".panel__btn", ".panel", ".panel__menu a");
+  relojDigital("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarma("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+});
