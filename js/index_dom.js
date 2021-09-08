5f9862d9ceacfd7761dbacb4688631e8c7b49d22
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-08 13:28:47 -0300

    countdown

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { relojDigital, alarma } from "./dom/reloj.js";
 import { atajos, moverPelota } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel__btn", ".panel", ".panel__menu a");
   relojDigital("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarma("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Sep 08,2022 12:22:00", "BOOM!!");
 });
 
 d.addEventListener("keydown", (e) => {
