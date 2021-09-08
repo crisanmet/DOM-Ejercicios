6b6f46ae86928973ca2298a332e3fab048c1cad3
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-08 14:03:57 -0300

    scroll top

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { relojDigital, alarma } from "./dom/reloj.js";
 import { atajos, moverPelota } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopButton from "./dom/boton_scroll.js";
 
 const d = document;
 
@@ -10,6 +11,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   relojDigital("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarma("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "Sep 08,2022 12:22:00", "BOOM!!");
+  scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
