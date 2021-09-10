9e73d2663bfc68316498735a9c60fc9d88582e19
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-10 16:54:23 -0300

    sorteo digital

--- js/index_dom.js
+++ js/index_dom.js
@@ -11,6 +11,7 @@ import networkStatus from "./dom/deteccion_red.js";
 import webCam from "./dom/deteccion_webcam.js";
 import getGeolocation from "./dom/geolocalizacion.js";
 import searchFilter from "./dom/filtro_busquedas.js";
+import draw from "./dom/sorteo.js";
 
 const d = document;
 
@@ -37,6 +38,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   webCam("webcam");
   getGeolocation("geolocation");
   searchFilter(".card-filter", ".card");
+  draw("#winner-btn", ".player");
 });
 
 d.addEventListener("keydown", (e) => {
